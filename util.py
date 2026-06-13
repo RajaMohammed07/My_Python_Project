d60ed5f62bbbf6736e639eef7198e9e8b431c5d5
Raja Mohammed <rajamohammed.m2021rvs@gmail.com>
2026-06-13 13:32:47 +0530

    added new feature

--- util.py
+++ util.py
@@ -1,5 +1,10 @@
 def main():
     print("This is the initial utility function v1.")
+    buggy_feature()
+
+def buggy_feature():
+    print("Executing new feature...")
+    x = 1 / 0
 
 if __name__ == "__main__":
     main()
